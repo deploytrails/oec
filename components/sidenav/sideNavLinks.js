c8bd2fc5c5d46857e6531ec3be0b2565382a1347
sampath n <deploytrails@gmail.com>
2020-10-20 19:25:43 +0530

    side Nav stage

--- components/sidenav/sideNavLinks.js
+++ components/sidenav/sideNavLinks.js
@@ -220,7 +220,7 @@ const SideNavLinks = () => {
 
         <li
           css={
-            router.pathname === "/cource-coordinator-allocation"
+            router.pathname === "/courseCoordinatorAllocation"
               ? css`
                   color: ${COLORS.GREEN};
                 `
@@ -229,7 +229,7 @@ const SideNavLinks = () => {
                 `
           }
         >
-          <Link href="/cource-coordinator-allocation">
+          <Link href="/courseCoordinatorAllocation">
             <a>
               <FontAwesomeIcon icon={faIdBadge} /> Cource Coordinator Allocation
             </a>
@@ -310,8 +310,22 @@ const SideNavLinks = () => {
                   </a>
                 </Link>
               </li>
-              <li>
-                <FontAwesomeIcon icon={faCompressArrowsAlt} /> Discrepancy
+              <li
+                css={
+                  router.pathname === "/mentoring/discrepancy"
+                    ? css`
+                        color: ${COLORS.GREEN};
+                      `
+                    : css`
+                        color: ${COLORS.TEXTGRAY};
+                      `
+                }
+              >
+                <Link href="/mentoring/discrepancy">
+                  <a>
+                    <FontAwesomeIcon icon={faCompressArrowsAlt} /> Discrepancy
+                  </a>
+                </Link>
               </li>
             </ul>
           )}
