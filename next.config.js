a13cb5577666cc682bdfeb4f4b87cda96a9d5f88
Anji <a.gunti@dickblick.com>
2020-11-09 13:56:07 +0530

    upgrdaded next version and icons in dashboard

--- next.config.js
+++ next.config.js
@@ -8,6 +8,13 @@ module.exports = {
   env: {
     APIBaseUrl: "http://15.206.189.30:8081/",
   },
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: ['http://p2w.in/'],
+    path: '/_next/image',
+    loader: 'imgix',
+  },
   webpack(config, options) {
     config.module.rules.push({
       test: /\.(png|jpg|jpeg|gif|svg|eot|ttf|woff|woff2)$/,
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -55,62 +55,62 @@ const Dashboard = () => {
     {
       name: "Dashboard",
       url: "/dashboard",
-      icon: <FontAwesomeIcon icon={faColumns} size="2x" />,
+      icon: <img src="/dash-icon-1.png" width="100" />,
     },
     {
       name: "Imports",
       url: "/imports",
-      icon: <FontAwesomeIcon icon={faFileImport} size="2x" />,
+      icon: <img src="/dash-icon-2.png" width="100" />,
     },
     {
       name: "Notifications",
       url: "/notifications",
-      icon: <FontAwesomeIcon icon={faBell} size="2x" />,
+      icon: <img src="/dash-icon-3.png" width="100" />,
     },
     {
       name: "Profile",
       url: "/profile",
-      icon: <FontAwesomeIcon icon={faUser} size="2x" />,
+      icon: <img src="/dash-icon-4.png" width="100" />,
     },
     {
       name: "Allocated Cources",
       url: "/allocated-cources/attendance",
-      icon: <FontAwesomeIcon icon={faList} size="2x" />,
+      icon: <img src="/dash-icon-5.png" width="100" />,
     },
     {
       name: "Cource Coordinator Allocation",
       url: "/cource-coordinator-allocation",
-      icon: <FontAwesomeIcon icon={faIdBadge} size="2x" />,
+      icon: <img src="/dash-icon-6.png" width="100" />,
     },
     {
       name: "Class Schedule",
       url: "/class-schedule",
-      icon: <FontAwesomeIcon icon={faCalendar} size="2x" />,
+      icon: <img src="/dash-icon-7.png" width="100" />,
     },
     {
       name: "Mentoring",
       url: "/dashboard",
-      icon: <FontAwesomeIcon icon={faKeyboard} size="2x" />,
+      icon: <img src="/dash-icon-2.png" width="100" />,
     },
     {
       name: "Exam Schedule",
       url: "/dashboard",
-      icon: <FontAwesomeIcon icon={faCalendar} size="2x" />,
+      icon: <img src="/dash-icon-8.png" width="100" />,
     },
     {
       name: "Reports",
       url: "/dashboard",
-      icon: <FontAwesomeIcon icon={faChartBar} size="2x" />,
+      icon: <img src="/dash-icon-9.png" width="100" />,
     },
     {
       name: "QueryForm",
       url: "/query-form",
-      icon: <FontAwesomeIcon icon={faPhone} size="2x" />,
+      icon: <img src="/dash-icon-10.png" width="100" />,
     },
     {
       name: "Skins",
       url: "/dashboard",
-      icon: <FontAwesomeIcon icon={faTint} size="2x" />,
+      icon: <img src="/dash-icon-1.png" width="100" />,
     },
   ];
 
@@ -121,22 +121,22 @@ const Dashboard = () => {
         <div className="clearfix">
           {dashBoardLinks &&
             dashBoardLinks.map((item) => (
-              <div className="w-3/12 float-left text-center relative">
+              <div className="w-4/12 float-left text-center relative">
                 <Link href={item.url} key={item.name}>
-                  <a className=" p-4 h-32 bg-white shadow  box-border block m-1 font-bold hover:shadow-2xl hover:text-green-400">
+                  <a className=" p-4 bg-white shadow  box-border block m-1 font-bold hover:shadow-2xl hover:text-green-400">
                     <span
                       css={css`
-                        position: absolute;
                         display: block;
-                        top: 50%;
-                        left: 50%;
-                        transform: translate(-50%, -50%);
                       `}
                     >
-                      {" "}
-                      <span className="block">{item.icon}</span>
+                      <span css={css`
+                        display:block;
+                        margin:0px auto;
+                        width:100px;
+                      `}>{item.icon}</span>
                       {item.name}
                     </span>
+                   
                   </a>
                 </Link>
               </div>
