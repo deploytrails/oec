c75d3cd809dd927c8936ee9415378ecef8f844a6
Anji <a.gunti@dickblick.com>
2020-11-09 17:15:04 +0530

    removed unused domain

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: ['http://p2w.in/'],
+    domains: [],
     path: '/_next/image',
     loader: 'imgix',
   },
